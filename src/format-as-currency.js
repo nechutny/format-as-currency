edf1c1853eeab51f08ae37aa074b226574fb6ca2
Stanislav Nechutný <stanislav@nechutny.net>
2023-02-15 13:23:09 +0100

    Allow to customize number of decimal places

--- src/format-as-currency.js
+++ src/format-as-currency.js
@@ -116,7 +116,7 @@ angular
       scope.$watch(function(){
 	    return scope.$eval(attrs.decimalPlaces)
       }, function (f) {
-	    if(f) {
+	    if(f !== undefined) {
 	      decimalPlaces = f
 	    } else {
 	      decimalPlaces = 2
